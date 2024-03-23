7bf2907ace0cb5c0fea291329abc21a843dceb92
SardorQobilov <sardorqobilov555@gmail.com>
2024-03-23 23:45:12 +0500

    change button compoent name

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,48 +1,7 @@
-// import { Button, Image } from "@chakra-ui/react";
-// import { Link } from "react-router-dom";
-// 0;
-// interface ButtonComponentProps {
-//   text: string;
-//   icon?: string;
-//   rightIcon?: string;
-//   to: string;
-//   colorScheme: string;
-//   width?: string;
-//   size: { base: string; md: string; desktop: string };
-//   leftIcon: React.ReactNode;
-// }
-
-// export const ButtonComponent: React.FC<ButtonComponentProps> = ({
-//   text,
-//   icon,
-//   rightIcon,
-//   to,
-//   ...rest
-// }) => {
-//   return (
-//     <Link to={to}>
-//       <Button display="flex" alignItems="center" gap="6px" {...rest}>
-//         {icon && <Image src={icon} />}
-//         <span>{text}</span>
-//         {rightIcon && <Image src={rightIcon} />}
-//       </Button>
-//     </Link>
-//   );
-// };
-
 import { Button, Image, SystemStyleObject } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import React from "react";
-
 interface ButtonComponentProps {
-  // text: string;
-  // icon?: string;
-  // rightIcon?: string;
-  // to: string;
-  // colorScheme: string;
-  // width?: string;
-  // size: { base: string; md: string; desktop: string };
-  // leftIcon: React.ReactNode;
   text: string;
   icon?: string;
   rightIcon?: string;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { ButtonComponent } from "./Button";
+export { ButtonComponent } from "./ButtonComponent";
 export { Header } from "./Header";
 export { Avito } from "./Avito";
 export { NavigationLink } from "./NavigationLink";
